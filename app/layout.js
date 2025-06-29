0a1c286bd8e526b0fdd2a6021e07798082f15cf1
Student mr-devloper005 <Student@mr.opxoo5@gmail.com>
2025-06-29 22:30:07 +0530

    refactor: enhance layout with metadata, update script import, and add sitemap and robots.txt

--- app/layout.js
+++ app/layout.js
@@ -7,19 +7,54 @@ import { Poppins } from "next/font/google";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import Script from "next/script";
 
 const rowdies = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 });
+
+export const metadata = {
+  title: "Pinpoint AI - Smart AI Workspace",
+  description:
+    "Use Pinpoint AI for instant chat, productivity, and smart learning.",
+  openGraph: {
+    title: "Pinpoint AI - Smart AI Workspace",
+    description: "Real-time AI chat, learning, and productivity.",
+    url: "https://pinpointaii.vercel.app",
+    siteName: "Pinpoint AI",
+    images: [
+      {
+        url: "ai.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Pinpoint AI - Smart AI Workspace",
+    description: "Real-time AI chat, learning, and productivity.",
+    images: ["https://pinpointaii.vercel.app/twitter-image.jpg"],
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="overflow-x-hidden">
       <head>
-        <script
+        <Script
           src="https://accounts.google.com/gsi/client"
           async
-          defer></script>
+          defer></Script>
+        <meta
+          name="description"
+          content="Pinpoint AI is a smart AI workspace for real-time chat, learning, and productivity. Experience instant inference with high accuracy."
+        />
+
+        <title>Pinpoint AI - Smart AI Workspace | Chat, Infer, Learn</title>
 
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"
